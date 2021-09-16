9efbedd862db8e072c7d264e29402842dc697105
whdi0404 <whdi0404@naver.com>
2021-09-17 02:07:38 +0900

    AI시스템 변경중

--- ProjectR/Assets/Scripts/AISystem/Work.cs
+++ ProjectR/Assets/Scripts/AISystem/Work.cs
@@ -1,86 +1,129 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class WorkSystem
-//{
-//    private List<WorkPlace> workPlaces = new List<WorkPlace>();
-//}
+public interface IRegionListener
+{
+    public void OnRegionChange(Vector2Int tileGroupIndex);
+}
 
-//public class WorkPlace
-//{
-//    public enum Direction
-//    { 
-//        Left = 1 << 0,
-//        Top = 1 << 1,
-//        Right = 1 << 2,
-//        Bottom = 1 << 3,
-//    }
+public interface IRObjectListener
+{
+    public void OnCreateObject(RObject rObject);
+    public void OnDestroyObject(RObject rObject);
+}
 
-//    private List<IWork> workList = new List<IWork>();
-//}
+public class WorkSystem : IRegionListener, IRObjectListener
+{
+    private Dictionary<LocalRegion, List<WorkPlace>> workPlaces;
 
-//public interface IWork
-//{ 
+    public void OnRegionChange(Vector2Int tileGroupIndex, List<LocalRegion> newRegions)
+    {
+        GameManager.Instance.RObjList
+    }
+    public void OnCreateObject(RObject rObject)
+    {
+        throw new System.NotImplementedException();
+    }
 
-//}
+    public void OnDestroyObject(RObject rObject)
+    {
+        throw new System.NotImplementedException();
+    }
+}
 
-//public class BuildWork : IWork
-//{
-//    public Inventory Inventory { get; private set; }
+public class WorkPlace
+{
+    public enum Direction
+    { 
+        Left = 1 << 0,
+        Top = 1 << 1,
+        Right = 1 << 2,
+        Bottom = 1 << 3,
+    }
 
-//    public float RemainWorkload { get; private set; }
+    private List<Work> workList = new List<Work>();
+}
 
-//    public bool IsFilledMaterials
-//    {
-//        get
-//        {
-//            foreach (var reqItem in planningDesc.ReqItemList)
-//            {
-//                if (Inventory.GetItemAmount(reqItem.ItemDesc) < reqItem.Amount)
-//                    return false;
-//            }
+public abstract class Work
+{
+    public Work()
 
-//            return true;
-//        }
-//    }
 
-//    public void Work(float work)
-//    {
-//        RemainWorkload -= work;
+    public abstract bool IsWorkable { get; }
 
-//        if (RemainWorkload <= 0)
-//        {
-//            Complete();
-//        }
-//    }
+    public float RemainWorkload { get; protected set; }
 
-//    public List<RequireItem> GetRemainReqItemList()
-//    {
-//        List<RequireItem> reqItemList = new List<RequireItem>();
-//        foreach (var reqItem in planningDesc.ReqItemList)
-//        {
-//            int remainAmount = reqItem.Amount - Inventory.GetItemAmount(reqItem.ItemDesc);
-//            if (remainAmount > 0)
-//                reqItemList.Add(new RequireItem() { ItemDesc = reqItem.ItemDesc, Amount = remainAmount });
-//        }
+    public void Cancel()
+    {
 
-//        return reqItemList;
-//    }
+    }
+    public virtual void Work(float work)
+    {
 
-//    public void Complete()
-//    {
-//        GameManager.Instance.DestroyRObject(this);
+    }
 
-//        if (PlanningDesc.Structure != null)
-//            GameManager.Instance.WorldMap.SetTile(MapTilePosition, PlanningDesc.Structure);
+    public virtual void Complete()
+    { 
+    
+    }
+}
 
-//        //if (planObject.PlanningDesc.InstallObject != null)
-//        //GameManager.Instance.CreateRObject(new InstallObject());
-//    }
+public class BuildWork : Work
+{
+    public StructurePlanningDescriptor PlanningDesc { get { return planningDesc; } }
 
-//    public bool AA()
-//    {
-        
-//    }
-//}
+    private StructurePlanningDescriptor planningDesc;
+    public BuildWork(StructurePlanningDescriptor planningDesc)
+    {
+        this.planningDesc = planningDesc;
+    }
+
+    public Inventory Inventory { get; private set; }
+
+    public override bool IsWorkable
+    {
+        get
+        {
+            foreach (var reqItem in planningDesc.ReqItemList)
+            {
+                if (Inventory.GetItemAmount(reqItem.ItemDesc) < reqItem.Amount)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    public void Work(float work)
+    {
+        RemainWorkload -= work;
+
+        if (RemainWorkload <= 0)
+        {
+            Complete();
+        }
+    }
+
+    public List<RequireItem> GetRemainReqItemList()
+    {
+        List<RequireItem> reqItemList = new List<RequireItem>();
+        foreach (var reqItem in planningDesc.ReqItemList)
+        {
+            int remainAmount = reqItem.Amount - Inventory.GetItemAmount(reqItem.ItemDesc);
+            if (remainAmount > 0)
+                reqItemList.Add(new RequireItem() { ItemDesc = reqItem.ItemDesc, Amount = remainAmount });
+        }
+
+        return reqItemList;
+    }
+
+    public void Complete()
+    {
+        if (PlanningDesc.Structure != null)
+            GameManager.Instance.WorldMap.SetTile(MapTilePosition, PlanningDesc.Structure);
+
+        //if (planObject.PlanningDesc.InstallObject != null)
+        //GameManager.Instance.CreateRObject(new InstallObject());
+    }
+}
